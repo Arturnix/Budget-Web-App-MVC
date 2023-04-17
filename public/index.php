d1b3033a6d24ccf30bdd567ec8d56e4b17119839
Arturnix <artur.zgodka.programista@gmail.com>
2023-04-18 00:01:54 +0200

    Add success account activation page

--- public/index.php
+++ public/index.php
@@ -21,6 +21,12 @@ set_exception_handler('Core\Error::exceptionHandler');
 
 
 /**
+ * Sessions
+ */
+session_start();
+
+
+/**
  * Routing
  */
 $router = new Core\Router();
@@ -29,6 +35,8 @@ $router = new Core\Router();
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('login', ['controller' => 'Login', 'action' => 'new']);
 $router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
+$router->add('password/reset/{token:[\da-f]+}', ['controller' => 'Password', 'action' => 'reset']);
+$router->add('signup/activate/{token:[\da-f]+}', ['controller' => 'Signup', 'action' => 'activate']);
 $router->add('{controller}/{action}');
-    
+
 $router->dispatch($_SERVER['QUERY_STRING']);
