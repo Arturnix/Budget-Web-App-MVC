336ae92c15381ecd8ea636c88cb5c80cfc7c8b7e
Arturnix <artur.zgodka.programista@gmail.com>
2023-05-04 16:12:09 +0200

    Customize reset password email format

--- App/Models/User.php
+++ App/Models/User.php
@@ -308,7 +308,7 @@ class User extends \Core\Model
         $text = View::getTemplate('Password/reset_email.txt', ['url' => $url]);
         $html = View::getTemplate('Password/reset_email.html', ['url' => $url]);
 
-        Mail::send($this->email, 'Password reset', $text, $html);
+        Mail::send($this->email, 'Twój Budżet - Reset hasła', $text, $html);
     }
 
     /**
